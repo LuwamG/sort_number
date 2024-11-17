a631ab5c4e94350954ba897b0e3e23e3ad31d8b0
LuwamG <30065081@cc.peralta.edu>
2024-11-16 23:07:36 -0800

    initial commit

---
+++ sort_number.hpp
@@ -0,0 +1,8 @@
+#ifndef SORT_NUMBER_HPP
+#define SORT_NUMBER_HPP
+#include <iostream>
+using namespace std;
+void display_array(int arr[], int size);
+void sort_numbers(int arr[], int size);
+void swap_fun(int& x, int& y);
+#endif
